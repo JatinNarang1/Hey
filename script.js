b83cce0fd35d2386a63b63945554f0ccc2066cf9
Jatin Narang <94895227+JatinNarang1@users.noreply.github.com>
2024-01-14 17:07:38 +0530

    Pareena-Page

---
+++ script.js
@@ -0,0 +1,38 @@
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Let's plan a date at Sundar Nursey";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "It's sad par confetti anyways";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "No worries, You just saved yorself from being kidnapped?";
+    showConfetti()
+
+}
+  
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
